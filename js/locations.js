a2551ab1afdab4cceff4237869e756dd9b2d3039
Bo Pang <brucepang96@gmail.com>
2018-02-01 09:20:36 -0800

    first commit

---
+++ js/locations.js
@@ -0,0 +1,7 @@
+var locations = [
+	{title: 'Geisel Library', location: {lat: 32.881334, lng: -117.237465}},
+	{title: 'Whole Foods', location: {lat: 32.868823, lng: -117.230593}},
+	{title: 'AMC Theater', location: {lat: 32.864923, lng: -117.232122}},
+	{title: 'Westfield UTC', location: {lat: 32.870975, lng: -117.210796}},
+	{title: 'La Regencia', location: {lat: 32.864367, lng: -117.224755}}
+];
